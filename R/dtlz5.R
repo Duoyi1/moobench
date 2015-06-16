caebccc34b45c1cb96ebdc571663cb8edf9b35fb
Jonas Muench <jonas.muench@tu-dortmund.de>
2015-06-16 11:11:31 +0200

    clean up dtlz

--- R/dtlz5.R
+++ R/dtlz5.R
@@ -2,8 +2,11 @@ dtlz5_wir = function(x, in.dim, out.dim) {
   x.head = x[seq_len(out.dim - 1)]
   x.tail = x[out.dim:length(x)] - 0.5
   
-  theta = (pi / (4 * (1 + sum(x.tail^2)))) * (1 + 2 * sum(x.tail^2) * x.head) * 0.5 * pi
-  #theta = ((0.5 * pi^2) / (4 * (1 + sum(x.tail^2)))) * (1 + 2 * sum(x.tail^2) * x.head)
+  g = sum(x.tail)^0.1
   
-  rev((1 + sum(x.tail^2)) * c(sin(theta), 1) * c(1, cumprod(cos(theta))))
+  theta = numeric(out.dim - 1)
+  theta[1] = x.head[1] * 0.5 * pi
+  theta[2:(out.dim - 1)] = (pi / (4 * (1 + g))) * (1 + 2 * g * x.head[2:(out.dim - 1)]) * 0.5 * pi
+
+  rev((1 + g) * c(sin(theta), 1) * c(1, cumprod(cos(theta))))
 }
--- R/dtlz6.R
+++ R/dtlz6.R
@@ -2,8 +2,9 @@ dtlz6_wir = function(x, in.dim, out.dim) {
   x.head = x[seq_len(out.dim - 1)]
   x.tail = x[out.dim:length(x)]
   
-  theta = (pi / (4 * (1 + sum(x.tail^0.1)))) * (1 + 2 * sum(x.tail^0.1) * x.head) * 0.5 * pi
-  #theta = ((0.5 * pi^2) / (4 * (1 + sum(x.tail^0.1)))) * (1 + 2 * sum(x.tail^0.1) * x.head)
+  g = 1 + (9 / length(x.tail)) * sum(x.tail)
+  h = length(x) - sum((x.head / (1 + g)) * (1 + sin(3 * pi * x.head)))
+  fm = (1 + g) * h
   
-  rev((1 + sum(x.tail^0.1)) * c(sin(theta), 1) * c(1, cumprod(cos(theta))))
+  c(x.head, fm)
 }
