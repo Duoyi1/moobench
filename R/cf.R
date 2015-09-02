655083d5a984d090af96c1369a57c6c2de23fdc7
Jonas Münch <jonas.muench@tu-dortmund.de>
2015-09-02 09:41:34 +0200

    cf2-5

--- R/cf.R
+++ R/cf.R
@@ -22,7 +22,12 @@ generateCF = function(id, in.dim = 30L, out.dim = 2L) {
   param.set = makeCFParamset(id, in.dim)
   
   fun = switch(id,
-    lz1
+    lz1,
+    lz2,
+    cf3,
+    cf4,
+    cf5,
+    cf6
   )
   
   mooFunction(
@@ -44,10 +49,108 @@ makeCFParamset = function(id, in.dim) {
       forbidden = expression({
         #FIXME: Ask someone intelligent!! This is not good!!!
         if (is.list(x))
-          x = x[[1]]
+          x = x[[1L]]
         f = lz1(x)
-        f[1] + f[2] - abs(sin(10 * pi * (f[1] - f[2] + 1))) - 1 >= 0
+        f[1L] + f[2L] - abs(sin(10 * pi * (f[1L] - f[2L] + 1))) - 1 >= 0
+      })),
+    makeParamSet(
+      makeNumericVectorParam(id = "x", len = in.dim, lower = 0, upper = 1),
+      forbidden = expression({
+        #FIXME: Ask someone intelligent!! This is not good!!!
+        if (is.list(x))
+          x = x[[1L]]
+        f = lz2(x)
+        tt = f[2L] + sqrt(f[1L]) - sin(2 * pi * (sqrt(f[1L]) - f[2L] + 1)) - 1
+        tt / (1 + exp(4 * abs(tt))) >= 0
+      })),
+    makeParamSet(
+      makeNumericVectorParam(id = "x", len = in.dim, lower = 0, upper = 1),
+      forbidden = expression({
+        #FIXME: Ask someone intelligent!! This is not good!!!
+        if (is.list(x))
+          x = x[[1L]]
+        f = cf3(x)
+        f[2L] + f[1L]^2 - sin(2 * pi * (f[1L]^2 - f[2L] + 1)) - 1 >= 0
+      })),
+    makeParamSet(
+      makeNumericVectorParam(id = "x", len = in.dim, lower = 0, upper = 1),
+      forbidden = expression({
+        #FIXME: Ask someone intelligent!! This is not good!!!
+        if (is.list(x))
+          x = x[[1L]]
+        tt = x[2L] - sin(6 * pi * x[1L] + 0.2 * pi) - 0.5 * x[1L] + 0.25
+        tt / (1 + exp(4 * abs(tt))) >= 0
+      })),
+    makeParamSet(
+      makeNumericVectorParam(id = "x", len = in.dim, lower = 0, upper = 1),
+      forbidden = expression({
+        #FIXME: Ask someone intelligent!! This is not good!!!
+        if (is.list(x))
+          x = x[[1L]]
+        x[2L] - 0.8 * x[1L] * sin(6 * pi * x[1L] + 0.2 * pi) - 0.5 * x[1L] + 0.25 >= 0
+      })),
+    makeParamSet(
+      makeNumericVectorParam(id = "x", len = in.dim, lower = 0, upper = 1),
+      forbidden = expression({
+        #FIXME: Ask someone intelligent!! This is not good!!!
+        if (is.list(x))
+          x = x[[1L]]
+        x[2L] - 0.8 * x[1L] * sin(6 * pi * x[1L] + 0.2 * pi) - sign(0.5 * (1 - x[1L]) - 
+            (1 - x[1L])^2) * sqrt(abs(0.5 * (1 - x[1L]) - (1 - x[1L])^2)) >= 0 &
+        x[4L] - 0.8 * x[1L] * sin(6 * pi * x[1L] + 0.4 * pi) - sign(0.25 * sqrt(1 - x[1L]) - 
+            0.5 * (1 - x[1L])) * sqrt(abs(0.25 * sqrt(1 - x[1L]) - 0.5 * (1 - x[1L]))) >= 0
       }))
   )
 }
 
+# definition of cf3-5
+cf3 = function(x, out.dim) {
+  j = 2:length(x)
+  j1 = j[j %% 2 == 1L]
+  j2 = j[j %% 2 == 0L]
+  
+  y = function(j) {
+    x[j] - x[1L]^(0.5 * (1 + (3 * (j - length(x))) / (length(x) - 2)))
+  }
+  
+  f1 = x[1L] + 2 / length(j1) * 
+    (4 * sum(y(j1)^2) - 2 * prod(cos((20 * y(j1) * pi) / (sqrt(j1)))) + 2)
+  f2 = 1 - x[1L]^2 + 2 / length(j2) * 
+    (4 * sum(y(j2)^2) - 2 * prod(cos((20 * y(j2) * pi) / (sqrt(j2)))) + 2)
+  return(c(f1, f2))
+}
+
+cf4 = function(x, out.dim) {
+  j = 2:length(x)
+  j1 = j[j %% 2 == 1L]
+  j2 = j[j %% 2 == 0L]
+  
+  y = function(j) {
+    x[j] - sin(6 * pi * x[1L] + (j * pi) / length(x))
+  }
+  
+  f1 = x[1L] + sum(y(j1)^2)
+  if (y(j2)[1L] < ((3 / 2) * (1 - sqrt(2) / 2))) {
+    f2 = 1 - x[1L] + sum(c(abs(y(j2)[1L]), y(j2)[-1L]^2))
+  }
+  else
+    f2 = 1 - x[1L] + sum(c(0.125 + (y(j2)[1L] - 1)^2, y(j2)[-1L]^2))
+  return(c(f1, f2))
+}
+
+cf5 = function(x, out.dim) {
+  j = 2:length(x)
+  j1 = j[j %% 2 == 1L]
+  j2 = j[j %% 2 == 0L]
+  
+  y1 = x[j1] - 0.8 * cos(6 * pi * x[1L] + (j1 * pi) / length(x))
+  y2 = x[j2] - 0.8 * sin(6 * pi * x[1L] + (j2 * pi) / length(x))
+  
+  f1 = x[1L] + sum(2 * y1^2 - cos(4 * pi * y1) + 1)
+  if (y2[1] < ((3 / 2) * (1 - sqrt(2) / 2))) {
+    f2 = 1 - x[1L] + sum(c(abs(y2[1L]), 2 * y2[-1L]^2 - cos(4 * pi * y2[-2L]) + 1))
+  }
+  else
+    f2 = 1 - x[1L] + sum(c(0.125 + (y2[1L] - 1)^2, 2 * y2[-1L]^2 - cos(4 * pi * y2[-2L]) + 1))
+  return(c(f1, f2))
+}
