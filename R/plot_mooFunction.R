caccaf6446ef2bb957bf5cd527966d583083221f
Karin Schork <schork.karin@gmail.com>
2015-10-16 15:39:41 +0200

    plotMooFunction: improve legend

--- R/plot_mooFunction.R
+++ R/plot_mooFunction.R
@@ -2,6 +2,16 @@
 #'
 #' @param x [\code{function}] \cr 
 #'  A \code{\link{mooFunction}}.
+#' @param resolution [\code{integer(1)}] \cr
+#'  Resolution for the plots. Default is 33.
+#' @param plot.vars [\code{integer(2)}]\cr
+#'  Indices for the two variables that should be plotted. Default are the first 
+#'  two variables.
+#' @param const.values [\code{numeric}] \cr
+#'  Numeric vector with length "in.dim - 2" (one entry for each variable that is 
+#'  not plotted). All variables which are not mentioned in \code{plot.vars} will 
+#'  be set to a constant value given by \code{const.values}. By default the lower
+#'  bounds of the variables are used.
 #' @param ... Ignored.
 #' 
 #' @export
@@ -10,7 +20,8 @@
 plot.mooFunction = function(x, ...) {
   plots = renderPlotMooFunction(x, ...)
   if (!is.null(plots$p.legend2)) {
-    p.legend = gridExtra::arrangeGrob(plots$p.legend1, plots$p.legend2, nrow = 2)
+    p.legend = gridExtra::arrangeGrob(plots$p.legend1, plots$p.legend2, nrow = 2, 
+      heights = c(0.75, 0.25))
   } else {
     p.legend = plots$p.legend1
   }
@@ -22,17 +33,18 @@ renderPlotMooFunction = function(x, resolution = 33L, plot.vars = 1:2, const.val
   
   in.dim = getInDim(x)
   
+  # checks
   if (in.dim < 2 || getOutDim(x) != 2) {
     stop("Only functions with InDim >= 2 and OutDim = 2 can be plotted!")
   }
-  
   assertIntegerish(resolution, lower = 2L, len = 1L)
   assertIntegerish(plot.vars, lower = 1L, upper = in.dim, len = 2L)
   if (!is.null(const.values))
     assertNumeric(const.values, len = in.dim - 2L)
   
   
-  renderBasicPlot = function(dat, title, legend = FALSE, ...) {
+  # function to render the main and the legend plot
+  renderBasicPlot = function(dat, title, legend = FALSE, res = resolution, ...) {
     dat$f1 = BBmisc::normalize(dat$f1, method = "range", range = c(0, 1))
     dat$f2 = BBmisc::normalize(dat$f2, method = "range", range = c(0, 1))
     
@@ -40,8 +52,6 @@ renderPlotMooFunction = function(x, resolution = 33L, plot.vars = 1:2, const.val
     dat$fill = as.factor(dat$fill)
     levels(dat$fill) = c("f1", "f2")
     dat[is.na(dat$f1), "fill"] = NA
-    #dat[is.na(dat$z1), ]$fill = rep("NA", sum(is.na(dat)))
-
 
     p = ggplot(aes = aes(x = x1, y = x2))
     
@@ -67,7 +77,7 @@ renderPlotMooFunction = function(x, resolution = 33L, plot.vars = 1:2, const.val
     p = p + ggtitle(title)
     p
     
-    if(legend == TRUE) {
+    if (legend == TRUE) {
       p = p + xlab("f1") + ylab("f2")
     } else {
       p = p + xlab(paste("x", plot.vars[1L], sep = "")) + ylab(paste("x", plot.vars[2L], sep = ""))
@@ -75,13 +85,6 @@ renderPlotMooFunction = function(x, resolution = 33L, plot.vars = 1:2, const.val
     
     return(p)
   }
-  
-  g_legend = function(a.gplot) { 
-    tmp = ggplot_gtable(ggplot_build(a.gplot)) 
-    leg = which(sapply(tmp$grobs, function(x) x$name) == "guide-box") 
-    legend = tmp$grobs[[leg]] 
-    return(legend)
-  } 
   
   fun = x
   par.set = getParamSet(fun)
@@ -93,6 +96,7 @@ renderPlotMooFunction = function(x, resolution = 33L, plot.vars = 1:2, const.val
     const.values = lower[-plot.vars]
   }
   
+  # generate grids
   grid = expand.grid(seq(lower[plot.vars[1L]], upper[plot.vars[1L]], length.out = resolution), 
     seq(lower[plot.vars[2L]], upper[plot.vars[2L]], length.out = resolution))
   grid2 = matrix(nrow = resolution ^ 2, ncol = getInDim(fun))
@@ -104,6 +108,7 @@ renderPlotMooFunction = function(x, resolution = 33L, plot.vars = 1:2, const.val
   dat = cbind(grid, t(z))
   names(dat) = c("x1", "x2", "f1", "f2")  
 
+  # render main plot
   p = renderBasicPlot(dat, title = name, ...) 
   
   # add pareto set
@@ -111,15 +116,29 @@ renderPlotMooFunction = function(x, resolution = 33L, plot.vars = 1:2, const.val
   if (nrow(pareto) > 0) {
     names(pareto) = c("x1", "x2")
     p = p + geom_point(data = pareto, aes(x = x1, y = x2, shape = "pareto set"))
-    # extract legend from plot
-    p.tmp = p + scale_shape_manual(name = "", values = c(16))
-    p.legend2 = g_legend(p.tmp)
     p = p + scale_shape_manual(guide = FALSE, values = c(16))
-  } else {
-    p.legend2 = NULL
   }
 
-  # legend
+  # function to extract legend from plot
+  g_legend = function(a.gplot) { 
+    tmp = ggplot_gtable(ggplot_build(a.gplot)) 
+    leg = which(sapply(tmp$grobs, function(x) x$name) == "guide-box") 
+    legend = tmp$grobs[[leg]] 
+    return(legend)
+  } 
+  
+  # generate small extra plot which is only used to extract the legend
+  dat.tmp = data.frame(x = c(1,2), y = c(1,1), fill = as.factor(c(1,1)))
+  p.tmp = ggplot(data = dat.tmp, aes(x = x, y = y, fill = fill, shape = fill)) + 
+    geom_tile(alpha = 0.25) + 
+    geom_point(colour = "black")
+  p.tmp = p.tmp + scale_fill_manual(name = "", values = c("green"), labels = "infeasible value") + 
+    scale_shape_manual(name = "  ", values = c(16), labels = "pareto front") +
+    guides(fill = guide_legend(override.aes = list(shape = NA)))
+  # extract legend from plot
+  p.legend2 = g_legend(p.tmp)
+  
+  # legend plot
   lower.legend = c(min(dat$f1, na.rm = TRUE), min(dat$f2, na.rm = TRUE))
   upper.legend = c(max(dat$f1, na.rm = TRUE), max(dat$f2, na.rm = TRUE))
   grid.legend = expand.grid(seq(lower.legend[1L], upper.legend[1L], length.out = 50L), 
